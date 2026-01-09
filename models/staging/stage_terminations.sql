597a10fb61487350193d755487a4e26be827a6ba
Zachary Regester <regesterhome@gmail.com>
2026-01-09 02:45:54 +0000

    Updating stage_terminations spacing for readability

--- models/staging/stage_terminations.sql
+++ models/staging/stage_terminations.sql
@@ -2,29 +2,29 @@
 
 select employee_id
 
-       --correcting odd date formatting in source data
+       -- Correcting odd date formatting in source data
     ,  (case when year(period_date) < 100
-            then dateadd(year, 2000, period_date)
-            else period_date end) as period_date_clean
+             then dateadd(year, 2000, period_date)
+             else period_date end) as period_date_clean
 
     ,  date_trunc('quarter', period_date_clean) as beg_of_quarter
 
     ,  (case when year(termination_date) < 100
-            then dateadd(year, 2000, termination_date)
-            else termination_date end) as termination_date_clean
+             then dateadd(year, 2000, termination_date)
+             else termination_date end) as termination_date_clean
 
-    ,  case when metric ilike 'Term%' then 'Termination'
-             else initcap(lower(metric)) end
+    ,  (case when metric ilike 'Term%' then 'Termination'
+             else initcap(lower(metric)) end)
              as metric
 
     ,  initcap(lower(job_name)) as job_name_clean
     ,  job_name_code
     ,  concat(job_name_clean, ' - ', job_name_code) as job_description
 
-       --correcting classification of contractor employee_ids
-    ,  case when employee_id like '%101%' and length(employee_id) >= 5 then 'True'
+       -- Correcting classification of contractor employee_ids
+    ,  (case when employee_id like '%101%' and length(employee_id) >= 5 then 'True'
             when employee_id in ('10256','10257', '10258') and period_date = '2025-08-01' then 'true'
-            else initcap(upper(is_contractor)) end as is_contractor_clean
+            else initcap(upper(is_contractor)) end) as is_contractor_clean
 
     ,  exec_leader
     ,  l1_from_top_leader
