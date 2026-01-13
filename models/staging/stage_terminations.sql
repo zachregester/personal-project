2da23581f22baf1fae9bab9def7188f2499df200
Zachary Regester <regesterhome@gmail.com>
2026-01-13 01:11:15 +0000

    Updating field names

--- models/staging/stage_terminations.sql
+++ models/staging/stage_terminations.sql
@@ -6,22 +6,24 @@ with base as (
 
         ,  (case when year(period_date) < 100 then dateadd(year, 2000, period_date)
                  else period_date
-                 end) as period_date_clean
+                 end) as period_date
 
         ,  date_trunc('quarter', period_date_clean) as beg_of_quarter
 
         ,  (case when year(termination_date) < 100
                  then dateadd(year, 2000, termination_date)
-                 else termination_date end) as termination_date_clean
+                 else termination_date end) as termination_date
 
         ,  initcap(lower(termination_type)) as termination_type
-        ,  termination_reason
+
+        ,  (case when termination_reason is null then 'Other'
+                 else termination_reason end) as termination_reason
 
         ,  (case when metric ilike 'Term%' then 'Termination'
                  else initcap(lower(metric)) end)
                  as metric
 
-        ,  initcap(lower(job_name)) as job_name_clean
+        ,  initcap(lower(job_name)) as job_name
 
         ,  job_name_code as job_code
 
@@ -29,7 +31,7 @@ with base as (
 
         ,  (case when employee_id like '999%' and length(employee_id) >= 5 then 'true'
                  when employee_id in ('10256','10257', '10258') and period_date = '2025-08-01' then 'true'
-                 else upper(try_to_boolean(is_contractor)) end) as is_contractor_clean
+                 else upper(try_to_boolean(is_contractor)) end) as is_contractor
 
         ,  exec_leader
         ,  l1_from_top_leader
